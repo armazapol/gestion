98aa233c0cf33e7d485adcd175db112bed4e88e1
pol <pol.armaza.002@gmail.com>
2023-11-04 01:25:51 -0500

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 // main.tsx or main.jsx
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
 import App from "./App";
 import { TanStackProvider } from "./plugins/TanStackProvider";
 import "./index.css";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-      <TanStackProvider>
-        <NextUIProvider>
-          <App />
-        </NextUIProvider>
-      </TanStackProvider>
+  <React.StrictMode>
+    <TanStackProvider>
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
+    </TanStackProvider>
+  </React.StrictMode>
 );
